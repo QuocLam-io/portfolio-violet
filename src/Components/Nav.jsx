df22dc8e37c4c75fc82382b86ac59fbbe4aacb89
frostycakes <lamquoc.24@gmail.com>
2022-12-10 09:20:14 -0500

    add navbar skeleton bones

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Nav = () => {
-  return <div className="nav-parent">Nav</div>;
+  return (
+    <div className="nav-parent">
+      <div className="nav-top">
+        <div className="logo"></div>
+        <div className="nav-links"></div>
+      </div>
+      <div className="nav-bottom">Hand-coded with love</div>
+    </div>
+  );
 };
 
 export default Nav;
