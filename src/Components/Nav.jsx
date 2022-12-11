b9e022e35a7884a0cdc6e99e0c5a42855afb33b1
frostycakes <lamquoc.24@gmail.com>
2022-12-10 21:17:09 -0500

    nav right border linear gradient 100% vertical

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <div className="nav-parent">
       <div className="nav-top">
         <div className="logo"></div>
-        <div className="nav-links"></div>
+        <div className="nav-menu"></div>
       </div>
       <div className="nav-bottom">Hand-coded with love</div>
     </div>
