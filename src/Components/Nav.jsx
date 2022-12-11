01b60534c9f35eba42401a90a643f84d30c20bb5
frostycakes <lamquoc.24@gmail.com>
2022-12-11 16:42:06 -0500

    navbar padding

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Merp from "../Components/Merp";
 
 const Nav = () => {
   return (
     <div className="nav-parent">
       <div className="nav-top">
-        <div className="logo"></div>
+        <div className="logo">
+          <Merp />
+        </div>
         <div className="nav-menu"></div>
       </div>
       <div className="nav-bottom">Hand-coded with love</div>
