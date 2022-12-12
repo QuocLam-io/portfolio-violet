487118def769226489e8a4ffbf3efaa584eb9dda
frostycakes <lamquoc.24@gmail.com>
2022-12-12 15:46:00 -0500

    logo font sizes

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,13 +7,16 @@ const Nav = () => {
       <div className="nav-top">
         <div className="logo">
           <Merp />
-          <div className="logo-right"></div>
+          <div className="logo-right">
+            <div className="">Quoc</div>
+            <div className="">Software Engineer</div>
+          </div>
         </div>
         <div className="nav-menu"></div>
       </div>
       <div className="nav-bottom">
         <p>Hand-coded with love</p>
-        {/* <img src="imgs/hollow-heart.svg" alt="" srcset="" /> */}
+        <img src="imgs/hollow-heart.svg" alt="" srcset="" />
       </div>
     </div>
   );
