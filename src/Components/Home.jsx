d76a08d694ba88357e13b7733a5ed285557a5d04
frostycakes <lamquoc.24@gmail.com>
2022-12-12 21:33:16 -0500

    Add icons to logo

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -32,9 +32,21 @@ const Home = () => {
           <p>Quoc Lam 👋🏻</p>
           <div>
             {/* <span ref={typedRef}></span> */}
+            <span>Fullstack Developer</span>
           </div>
         </div>
-        <div className="name-icons">Icons</div>
+        <div className="name-icons">
+          <img src="imgs/icons/html.svg" alt="" />
+          <img src="imgs/icons/css.svg" alt="" />
+          <img src="imgs/icons/js.svg" alt="" />
+          <img src="imgs/icons/mongodb.svg" alt="" />
+          <img src="imgs/icons/express.svg" alt="" />
+          <img src="imgs/icons/react.svg" alt="" />
+          <img src="imgs/icons/nodejs.svg" alt="" />
+          <img src="imgs/icons/framer-motion.svg" alt="" />
+          <img src="imgs/icons/sass.svg" alt="" />
+          <p>Add Redux</p>
+        </div>
       </div>
     </div>
   );
