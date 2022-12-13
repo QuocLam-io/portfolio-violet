f5fbc745ee5252753dc3fc32c137bd81dbd77bb1
frostycakes <lamquoc.24@gmail.com>
2022-12-12 20:44:52 -0500

    Import Typed library

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+import Typed from "typed.js";
 
 const Home = () => {
-  return (
-    <div className='home-parent' >
-      
-      <div className="test"></div>
-    </div>
-  )
-}
+  // const typedRef = useRef();
 
-export default Home
+  // useEffect(() => {
+  //   const typed = new Typed(typedRef.current, {
+  //     strings: [
+  //       "Fullstack Developer,",
+  //       "Continuous Learner,",
+  //       "Cat Dad,",
+  //       "Fried Chicken Enthusiast",
+  //     ],
+  //     typeSpeed: 50,
+  //     backSpeed: 50,
+  //     backDelay: 1500,
+  //     loop: true,
+  //   });
+
+  //   return () => {
+  //     typed.destroy();
+  //   };
+  // }, []);
+
+  return (
+    <div className="home-parent">
+      <div className="home-name">
+        <div className="name-titles">
+          <p>Hey, I'm</p>
+          <p>Quoc Lam 👋🏻</p>
+          <div>
+            {/* <span ref={typedRef}></span> */}
+          </div>
+        </div>
+        <div className="name-icons">Icons</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
