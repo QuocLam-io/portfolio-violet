261e3d2a6e4e31fe7d94aa8e234d1f82e1f6158a
frostycakes <lamquoc.24@gmail.com>
2022-12-19 13:45:35 -0500

    import About Component to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Nav from "./Components/Nav";
 import Home from "./Components/Home";
+import About from "./Components/About";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+
       </Routes>
     </div>
   );
