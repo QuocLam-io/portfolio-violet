0ef746bab0beb28b671e1dd0258fe829e3ff2030
frostycakes <lamquoc.24@gmail.com>
2022-12-21 17:46:23 -0500

    About path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="about" element={<About />} />
       </Routes>
     </div>
   );
