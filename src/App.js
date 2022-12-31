890655ebb5eb63534c60550451c257a66ee71fd6
frostycakes <lamquoc.24@gmail.com>
2022-12-30 22:52:53 -0500

    navbar mobile top height

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+
+
+
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
