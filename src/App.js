d7cad2e13c611c083bc6337dcdc74acef6451190
frostycakes <lamquoc.24@gmail.com>
2023-01-09 15:37:13 -0500

    comment out for build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import NavBottom from "./Components/NavBottom";
-import Menu from "./Components/Menu";
+// import Menu from "./Components/Menu";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
