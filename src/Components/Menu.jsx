229046c2439346cb4171935521eb798fd9471ab6
frostycakes <lamquoc.24@gmail.com>
2023-01-27 21:10:57 -0500

    menu links

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
-  return <div className="Menu"
-  >
-    
-
-
-  </div>;
+  return (
+    <div className="Menu">
+      <Link to="/" className="nav-menu-item">
+        home
+      </Link>
+      <Link to="/about" className="nav-menu-item">
+        about
+      </Link>
+      {/* <Link to="/contact" className="nav-menu-item">
+            contact
+          </Link> */}
+    </div>
+  );
 };
 
 export default Menu;
